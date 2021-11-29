68f18ab1561a11314f46db4f6b5e2db1cf8deee9
Ramiro Aquistapace <ramiaquista70@gmail.com>
2021-11-29 14:25:15 -0300

    Add true in query instad of t, and fix query to find animals whith name ending in mon

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 
 /* Find all animals whose name ends in "mon". */
 
-SELECT * FROM animals WHERE name LIKE '%mon%';
+SELECT * FROM animals WHERE name LIKE '%mon';
 
 /* List the name of all animals born between 2016 and 2019. */
 
@@ -10,7 +10,7 @@ SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' and '2019-12-3
 
 /* List the name of all animals that are neutered and have less than 3 escape attempts. */
 
-SELECT name FROM animals WHERE neutered = 't' AND escape_attempts < 3;
+SELECT name FROM animals WHERE neutered = 'true' AND escape_attempts < 3;
 
 /* List date of birth of all animals named either "Agumon" or "Pikachu". */
 
@@ -22,7 +22,7 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 
 /* Find all animals that are neutered. */
 
-SELECT * FROM animals WHERE neutered = 't';
+SELECT * FROM animals WHERE neutered = 'true';
 
 /* Find all animals not named Gabumon. */
 
