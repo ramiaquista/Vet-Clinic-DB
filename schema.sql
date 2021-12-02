58d7781aeda73d85ec2a82bb32b447431f9dbd3d
Ramiro Aquistapace <ramiaquista70@gmail.com>
2021-12-02 12:27:41 -0300

    Create specializations and visits "joins tables" Insert data inside specializations and visits tables.

--- schema.sql
+++ schema.sql
@@ -22,9 +22,33 @@ CREATE TABLE species (
   id INT GENERATED ALWAYS AS IDENTITY,
   name VARCHAR(250),
   PRIMARY KEY (id),
-)
+);
 
 ALTER TABLE animals ADD PRIMARY KEY (id);
 ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
 ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
+
+CREATE TABLE vets (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(250),
+  age INT,
+  date_of_graduation DATE,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE specialiazations (
+  vet_id INT,
+  specie_id INT,
+  FOREIGN KEY (vet_id) REFERENCES vets(id),
+  FOREIGN KEY (specie_id) REFERENCES species(id)
+);
+
+CREATE TABLE visits (
+  animal_id INT,
+  vet_id INT,
+  date_of_visit DATE,
+  FOREIGN KEY (animal_id) REFERENCES animals(id),
+  FOREIGN KEY (vet_id) REFERENCES vets(id)
+);
+
